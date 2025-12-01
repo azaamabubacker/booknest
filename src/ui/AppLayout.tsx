ca350ffad108d6837f17122676b60d262d04a7a4
Azaam <cleancodelk@gmail.com>
2025-12-01 20:14:15 +0530

    chore(data) add @tanstack/react-query

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -3,9 +3,11 @@ import { Link, Outlet } from 'react-router-dom';
 export default function AppLayout() {
   return (
     <header>
-      <nav>
-        <div>
-          <Link to="/">BookNest</Link>
+      <nav className="flex justify-between">
+        <div className="flex gap-6 items-center">
+          <Link to="/" className="text-lg font-semibold text-foreground ">
+            BookNest
+          </Link>
           <Link to="/books">Books</Link>
         </div>
         <div>
