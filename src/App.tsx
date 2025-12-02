e72870807cdd3a4a9244b35a5ff4f8610ac62b5d
Azaam <cleancodelk@gmail.com>
2025-12-02 20:20:51 +0530

    feat(api) add json-server and data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/router';
 import './App.css';
-import { QueryClientProvider, useQuery } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClientBook } from './lib/query/client';
 
 export default function App() {
